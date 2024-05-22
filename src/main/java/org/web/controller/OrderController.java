b8fd8b1208c5504ba8d4f3ca406429c74c4243c5
jack <s80532s@gmail.com>
2024-05-22 19:28:15 +0800

    <feat> init order interface

---
+++ src/main/java/org/web/controller/OrderController.java
@@ -0,0 +1,22 @@
+package org.web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.web.dto.Result;
+
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+
+    @GetMapping
+    public Result test(){
+
+
+            }
+
+
+
+
+
+}
---
+++ src/main/java/org/web/entity/Order.java
@@ -0,0 +1,52 @@
+package org.web.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.Date;
+@Entity
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer orderId;
+
+    private Integer price;
+
+    private Date createDate;
+
+    private Integer userId;
+
+    public Integer getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Integer orderId) {
+        this.orderId = orderId;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+}
